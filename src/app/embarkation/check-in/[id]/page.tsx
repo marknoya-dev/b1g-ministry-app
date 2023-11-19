c11f91f3faa02140930cfd7c6e5c6a1bf4da1ae8
marknoya-dev <markbriannoya@gmail.com>
2023-11-19 23:21:20 +0800

    deleted check-in/id route

--- src/app/embarkation/check-in/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { getParticipantData } from "@/lib/api";
-
-export default async function Page(url: { params: { id: string } }) {
-  const { id } = url.params;
-  const data = await getParticipantData(id);
-
-  return <div>{"TEST"}</div>;
-}
