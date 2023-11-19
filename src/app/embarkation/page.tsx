a8eb79545dc7c863a03d7b29c434918a6b2fe6d7
marknoya-dev <markbriannoya@gmail.com>
2023-11-19 23:52:48 +0800

    Recommented in Embarkation Page

--- src/app/embarkation/page.tsx
+++ src/app/embarkation/page.tsx
@@ -15,48 +15,47 @@ export default async function Home() {
     return null;
   }
 
-  return <div>Test</div>;
-  // const [buses, participants] = await Promise.all([
-  //   getAllBusData(),
-  //   getParticipantsData(),
-  // ]);
+  const [buses, participants] = await Promise.all([
+    getAllBusData(),
+    getParticipantsData(),
+  ]);
 
-  // return (
-  //   <main>
-  //     <h1 className="text-[24px] font-bold mb-[16px] text-gray-700">
-  //       Embarkation
-  //     </h1>
+  return (
+    <main>
+      <h1 className="text-[24px] font-bold mb-[16px] text-gray-700">
+        Embarkation
+      </h1>
 
-  //     <div className="flex flex-col gap-4">
-  //       <Card className="shadow-md">
-  //         <CardHeader className="gap-4px">
-  //           <CardTitle className="text-lg">Bus Capacity</CardTitle>
-  //           <CardDescription className="mt-0">
-  //             Watch which buses are currently getting filled
-  //           </CardDescription>
-  //         </CardHeader>
-  //         <CardContent>
-  //           <div className="grid grid-cols-4 grid-rows-2 gap-3">
-  //             {buses.map((bus) => (
-  //               <CapacityCard
-  //                 label={bus.name}
-  //                 key={bus.name}
-  //                 value={bus.currCapacity}
-  //                 max={bus.maxCapacity}
-  //               />
-  //             ))}
-  //           </div>
-  //         </CardContent>
-  //       </Card>
-  //       <Card className="shadow-md">
-  //         <CardHeader>
-  //           <CardTitle className="text-lg">All Participants</CardTitle>
-  //         </CardHeader>
-  //         <CardContent>
-  //           <DataTable columns={columns} data={participants} />
-  //         </CardContent>
-  //       </Card>
-  //     </div>
-  //   </main>
-  // );
+      <div className="flex flex-col gap-4">
+        <Card className="shadow-md">
+          <CardHeader className="gap-4px">
+            <CardTitle className="text-lg">Bus Capacity</CardTitle>
+            <CardDescription className="mt-0">
+              Watch which buses are currently getting filled
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-4 grid-rows-2 gap-3">
+              {buses.map((bus) => (
+                <CapacityCard
+                  label={bus.name}
+                  key={bus.name}
+                  value={bus.currCapacity}
+                  max={bus.maxCapacity}
+                />
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="shadow-md">
+          <CardHeader>
+            <CardTitle className="text-lg">All Participants</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <DataTable columns={columns} data={participants} />
+          </CardContent>
+        </Card>
+      </div>
+    </main>
+  );
 }
